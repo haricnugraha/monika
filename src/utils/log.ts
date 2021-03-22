1a3be0a8a573f5eeef1b4fa912641aeaa2b048db
Armedi <armedi@outlook.com>
2021-03-22 20:28:21 +0700

    test: spy on pino log function

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,18 +1,18 @@
 import fs from 'fs'
 import path from 'path'
-import pino from 'pino'
+import pino, { LoggerOptions, PrettyOptions } from 'pino'
 
 const project = path.join(__dirname, '../../tsconfig.json')
 const dev = fs.existsSync(project)
 
-const transport = dev
+const transport: LoggerOptions = dev
   ? {
       prettyPrint: {
         colorize: true,
         translateTime: true,
         ignore: 'pid,hostname',
-      },
+      } as PrettyOptions,
     }
   : {}
 
-export const log = process.env.NODE_ENV === 'test' ? console : pino(transport)
+export const log = pino(transport)
